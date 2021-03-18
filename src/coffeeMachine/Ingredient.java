54bec6b2451a0384aebe8617c9c9962f0e12c5a0
Ambar <ambar.maini@razorpay.com>
2021-03-18 16:09:19 +0000

    Add ingredient class

---
+++ src/coffeeMachine/Ingredient.java
@@ -0,0 +1,15 @@
+package coffeeMachine;
+
+public class Ingredient {
+    String name;
+
+    public Ingredient(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+}
